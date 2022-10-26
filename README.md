e2527e6a11ac66116c34bd0a31d82da05e137a82
PatCook123 <cookpat@oregonstate.edu>
2022-10-26 11:42:57 -0600

    Debugged - was not finding index.html. Still isn't on venv

--- README.md
+++
@@ -1,6 +0,0 @@
-# Overview
-
-Group 89
-Cheyenne Plutchak & Patrick Cook
-
-This program is an implementation of a Pokemon SQL RDBMS
--- app.py
+++ app.py
@@ -1,26 +1,13 @@
-# Group 89
-# Patrick Cook & Cheyenne Plutchak
-
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap4
-from flask import Flask, render_template, url_for
-import os
+app = Flask(__name__)
 
-
-# Configuration
-
-app = Flask(__name__, template_folder = '/cs340_pokemondb/templates')
-"""db_connection = db.connect_to_database()"""
 bootstrap = Bootstrap4(app)
 
-# Routes 
-
 @app.route('/')
-def root():
-    return render_template("index.html")
+def homepage():  # put application's code here
+    return render_template('index.html')
 
 
-# Listener
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 50050))
-    app.run(port=port, debug=True)
+if __name__ == '__main__':
+    app.run(port=3000)
