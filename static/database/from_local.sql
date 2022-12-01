18c7dcbaef1000e384b929ac6f7b10a315c54ed6
PatCook123 <76977450+PatCook123@users.noreply.github.com>
2022-12-01 13:38:04 -0700

    Delete from_local.sql

--- static/database/from_local.sql
+++
@@ -1,6 +0,0 @@
-SELECT first_name, last_name, xp, gyms.gym_name AS gym, pokedecks.pokedeck_name AS pokedeck FROM trainers
-LEFT JOIN gyms ON gyms.gym_id = trainers.gyms_gym_id
-LEFT JOIN pokedecks ON pokedecks.trainers_trainer_id = trainers.trainer_id
-WHERE first_name LIKE '%He%' OR last_name LIKE '%He%'
-GROUP BY trainer_id
-ORDER BY trainer_id;
